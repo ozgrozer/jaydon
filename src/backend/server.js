72a8277346970954650c17a670fc432609c1286f
Ozgur Ozer <ozgr@live.com>
2019-09-07 23:54:30 +0300

    Add boilerplate.

---
+++ src/backend/server.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const session = require('express-session')
+const port = 1148
+
+app.use(express.json())
+app.set('views', path.join(__dirname, '..', 'frontend'))
+app.use(express.static(path.join(__dirname, '..', '..', 'dist')))
+app.set('view engine', 'pug')
+app.use(session({
+  resave: true,
+  key: 'jaydon',
+  saveUninitialized: true,
+  secret: '2$2tc^/Q2nv7y^Zi',
+  cookie: { maxAge: 2592000000 }
+}))
+
+app.listen(port, () => {
+  console.log('Example app listening on port http://localhost:' + port)
+})
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    defaults: {
+      isAuthenticated: req.session.isAuthenticated || false
+    }
+  })
+})
---
+++ src/frontend/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
