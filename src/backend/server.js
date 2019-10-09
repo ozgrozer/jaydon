2f08ed0ccd6dbffaa0051db357c3c96ab368ca47
Ozgur Ozer <ozgr@live.com>
2019-10-09 21:44:03 +0300

    Add sqlite.

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const sqlite3 = require('sqlite3')
 
 const defaults = require('./defaults')
 const siteRoutes = require('./routes/siteRoutes')
@@ -11,6 +12,13 @@ app.set('views', path.join(__dirname, '..', 'frontend', 'html'))
 
 const getTimeForConsole = () => new Date(Date.now()).toLocaleString() + ':'
 
-app.listen(defaults.site.port, () => {
-  console.log(getTimeForConsole(), `${defaults.site.name}: http://localhost:${defaults.site.port}`)
+const dbPath = path.join(__dirname, '..', '..', `${defaults.site.dbName}.sqlite`)
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) console.log(err)
+
+  console.log(getTimeForConsole(), `Connected to database ${dbPath}`)
+
+  app.listen(defaults.site.port, () => {
+    console.log(getTimeForConsole(), `App is running on http://localhost:${defaults.site.port}`)
+  })
 })
