77a41e4ef3864257fef753d0087852a0642444d1
Ozgur Ozer <ozgr@live.com>
2019-10-11 17:37:00 +0300

    Show some data.

--- src/backend/routes/apiRoutes/v1/domains/readDomain.js
+++ src/backend/routes/apiRoutes/v1/domains/readDomain.js
@@ -1,9 +1,31 @@
+const { dbGet, dbAll } = require.main.require('./db/db')
+
 const readDomain = async (req, res) => {
   const result = { success: false }
 
   try {
+    let getDomains = {}
+    const { data } = req.body
+    if (Object.prototype.hasOwnProperty.call(data, 'id')) {
+      const _getDomains = await dbGet({
+        query: `
+          select * from domains
+          where id='${data.id}'
+        `
+      })
+      getDomains = _getDomains.row
+    } else {
+      const _getDomains = await dbAll({
+        query: `
+          select * from domains
+        `
+      })
+      getDomains = _getDomains.rows
+    }
+
     result.success = true
     result.name = 'readDomain'
+    result.domains = getDomains
     res.json(result)
   } catch (err) {
     result.error = err.message
