fe2627c0ee80294495c4e85c9593c9214382b056
Ozgur Ozer <ozgr@live.com>
2019-10-14 12:04:21 +0300

    Domain can be created.

--- src/backend/routes/apiRoutes/v1/domains/createDomain.js
+++ src/backend/routes/apiRoutes/v1/domains/createDomain.js
@@ -1,9 +1,20 @@
+const { dbRun } = require.main.require('./db/db')
+
 const createDomain = async (req, res) => {
   const result = { success: false }
+  const { data } = req.body
 
   try {
+    const _createDomain = await dbRun({
+      query: 'insert into domains(domain, createdAt) values($domain, $createdAt)',
+      params: {
+        $domain: data.domain,
+        $createdAt: +new Date()
+      }
+    })
+
+    result.data = _createDomain.data
     result.success = true
-    result.name = 'createDomain'
     res.json(result)
   } catch (err) {
     result.error = err.message
