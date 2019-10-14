fca497876a7d600623e38524918a66a86d307023
Ozgur Ozer <ozgr@live.com>
2019-10-14 13:38:36 +0300

    Delete domain.

--- src/backend/routes/apiRoutes/v1/domains/deleteDomain.js
+++ src/backend/routes/apiRoutes/v1/domains/deleteDomain.js
@@ -1,12 +1,22 @@
+const { dbRun } = require.main.require('./db/db')
+
 const deleteDomain = async (req, res) => {
   const result = { success: false }
+  const { data } = req.body
 
   try {
+    const _deleteDomain = await dbRun({
+      query: 'delete from domains where id=$id',
+      params: {
+        $id: data.id
+      }
+    })
+
     result.success = true
-    result.name = 'deleteDomain'
+    result.data = _deleteDomain.data
     res.json(result)
   } catch (err) {
-    result.error = err.message
+    result.error = err.error
     res.json(result)
   }
 }
