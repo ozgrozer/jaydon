96974766eb4f2680e8555c6623d879f37060bb1f
Ozgur Ozer <ozgr@live.com>
2019-10-14 13:44:27 +0300

    Domain can be updated.

--- src/backend/routes/apiRoutes/v1/domains/updateDomain.js
+++ src/backend/routes/apiRoutes/v1/domains/updateDomain.js
@@ -1,12 +1,24 @@
+const { dbRun } = require.main.require('./db/db')
+
 const updateDomain = async (req, res) => {
   const result = { success: false }
+  const { data } = req.body
 
   try {
+    const _updateDomain = await dbRun({
+      query: 'update domains set domain=$domain, updatedAt=$updatedAt where id=$id',
+      params: {
+        $id: data.id,
+        $domain: data.domain,
+        $updatedAt: +new Date()
+      }
+    })
+
     result.success = true
-    result.name = 'updateDomain'
+    result.data = _updateDomain.data
     res.json(result)
   } catch (err) {
-    result.error = err.message
+    result.error = err.error
     res.json(result)
   }
 }
