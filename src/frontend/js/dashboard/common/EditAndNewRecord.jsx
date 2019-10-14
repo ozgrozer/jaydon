8731863aaca0bb473020b61254c11fa6b9bf147c
Ozgur Ozer <ozgr@live.com>
2019-10-14 16:56:44 +0300

    Combine edit and new record components into one component.

---
+++ src/frontend/js/dashboard/common/EditAndNewRecord.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+
+const EditAndNewRecord = props => {
+  const { component } = props
+  const recordId = props.match.params.recordId
+  const section = recordId === 'new' ? 'new' : 'edit'
+  const sectionTitle = section === 'new' ? 'New' : 'Edit'
+
+  useEffect(() => {
+    document.title = sectionTitle + ' - ' + window.defaults.routes[`/${component.pluralId}`].title
+  }, [])
+
+  return (
+    <div id='newRecord'>
+      <div className='header'>
+        <h1>{sectionTitle} {component.singularTitle}</h1>
+      </div>
+    </div>
+  )
+}
+
+export default EditAndNewRecord
--- src/frontend/js/dashboard/common/Page.jsx
+++ src/frontend/js/dashboard/common/Page.jsx
@@ -2,17 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 import ListRecords from './ListRecords'
-import NewRecord from './NewRecord'
-import RecordDetails from './RecordDetails'
-
-const RecordDetailsOrNewRecord = props => {
-  const recordId = props.match.params.recordId
-  if (recordId === 'new') {
-    return <NewRecord {...props} />
-  } else {
-    return <RecordDetails {...props} />
-  }
-}
+import EditAndNewRecord from './EditAndNewRecord'
 
 const Page = props => {
   return (
@@ -21,7 +11,7 @@ const Page = props => {
         exact
         path='/domains/:recordId'
         component={_props => (
-          <RecordDetailsOrNewRecord
+          <EditAndNewRecord
             {..._props}
             component={props.component}
           />
