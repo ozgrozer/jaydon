08b52d2ab4b71e53797e4e5c88955068dfb317c4
Ozgur Ozer <ozgr@live.com>
2019-10-17 14:33:32 +0300

    Add exec function.

---
+++ src/backend/common/exec.js
@@ -0,0 +1,13 @@
+const { exec } = require('child_process')
+
+const _exec = (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) reject(error)
+      if (stderr) reject(stderr)
+      resolve(stdout)
+    })
+  })
+}
+
+module.exports = _exec
