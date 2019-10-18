d507152ab63a2e9141230b82f4980548e6d54658
Ozgur Ozer <ozgr@live.com>
2019-10-18 14:46:08 +0300

    Make a better view.

--- src/backend/routes/apiRoutes/v1/domains/deleteDomain.js
+++ src/backend/routes/apiRoutes/v1/domains/deleteDomain.js
@@ -2,34 +2,56 @@ const exec = require.main.require('./common/exec')
 const defaults = require.main.require('./defaults')
 const { dbRun, dbGet } = require.main.require('./db/db')
 
+const getDomain = async props => {
+  const { domainId } = props
+  const getDomain = await dbGet({
+    query: `
+      select domain from domains
+      where id='${domainId}'
+    `
+  })
+  const domain = getDomain.row.domain
+  return domain
+}
+
+const deleteWwwDirectory = async props => {
+  const { domain } = props
+  const wwwDirectoryPath = `${defaults.nginx.dir.www}/${domain}`
+  const result = await exec(`rm -r ${wwwDirectoryPath}`)
+  return result
+}
+
+const deleteNginxConfFile = async props => {
+  const { domain } = props
+  const nginxConfFilePath = `${defaults.nginx.dir.core}/sites-available/${domain}`
+  const result = await exec(`rm ${nginxConfFilePath}`)
+  return result
+}
+
+const deleteDomainRow = async props => {
+  const { domainId } = props
+  const deleteRecord = await dbRun({
+    query: 'delete from domains where id=$id',
+    params: {
+      $id: domainId
+    }
+  })
+  return deleteRecord
+}
+
 const deleteDomain = async (req, res) => {
   const result = { success: false }
-  const { data } = req.body
 
   try {
-    const getDomain = await dbGet({
-      query: `
-        select domain from domains
-        where id='${data.id}'
-      `
-    })
-    const domain = getDomain.row.domain
+    const domainId = req.body.data.id
 
-    const wwwDirectoryPath = `${defaults.nginx.dir.www}/${domain}`
-    await exec(`rm -r ${wwwDirectoryPath}`)
-
-    const nginxConfFilePath = `${defaults.nginx.dir.core}/sites-available/${domain}`
-    await exec(`rm ${nginxConfFilePath}`)
-
-    const _deleteDomain = await dbRun({
-      query: 'delete from domains where id=$id',
-      params: {
-        $id: data.id
-      }
-    })
+    const domain = await getDomain({ domainId })
+    await deleteWwwDirectory({ domain })
+    await deleteNginxConfFile({ domain })
+    const _deleteDomainRow = await deleteDomainRow({ domainId })
 
     result.success = true
-    result.data = _deleteDomain.data
+    result.data = _deleteDomainRow.data
     res.json(result)
   } catch (err) {
     result.error = err.message
