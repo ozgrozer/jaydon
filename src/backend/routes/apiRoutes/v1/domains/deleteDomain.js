1e6b571d2955fec79b810394a87738568991028d
Ozgur Ozer <ozgr@live.com>
2019-10-18 14:15:14 +0300

    Delete nginx configuration.

--- src/backend/routes/apiRoutes/v1/domains/deleteDomain.js
+++ src/backend/routes/apiRoutes/v1/domains/deleteDomain.js
@@ -1,10 +1,26 @@
-const { dbRun } = require.main.require('./db/db')
+const exec = require.main.require('./common/exec')
+const defaults = require.main.require('./defaults')
+const { dbRun, dbGet } = require.main.require('./db/db')
 
 const deleteDomain = async (req, res) => {
   const result = { success: false }
   const { data } = req.body
 
   try {
+    const getDomain = await dbGet({
+      query: `
+        select domain from domains
+        where id='${data.id}'
+      `
+    })
+    const domain = getDomain.row.domain
+
+    const wwwDirectoryPath = `${defaults.nginx.dir.www}/${domain}`
+    await exec(`rm -r ${wwwDirectoryPath}`)
+
+    const nginxConfFilePath = `${defaults.nginx.dir.core}/sites-available/${domain}`
+    await exec(`rm ${nginxConfFilePath}`)
+
     const _deleteDomain = await dbRun({
       query: 'delete from domains where id=$id',
       params: {
@@ -16,7 +32,7 @@ const deleteDomain = async (req, res) => {
     result.data = _deleteDomain.data
     res.json(result)
   } catch (err) {
-    result.error = err.error
+    result.error = err.message
     res.json(result)
   }
 }
