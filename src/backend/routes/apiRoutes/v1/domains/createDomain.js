24c72133ac86370cc846bd99e2ce362418518901
Ozgur Ozer <ozgr@live.com>
2019-10-18 14:15:05 +0300

    Create nginx configuration.

--- src/backend/routes/apiRoutes/v1/domains/createDomain.js
+++ src/backend/routes/apiRoutes/v1/domains/createDomain.js
@@ -1,14 +1,25 @@
+const exec = require.main.require('./common/exec')
+const { nginxConfGen } = require.main.require('./common/nginx')
+const defaults = require.main.require('./defaults')
 const { dbRun } = require.main.require('./db/db')
 
 const createDomain = async (req, res) => {
   const result = { success: false }
   const { data } = req.body
+  const { domain } = data
 
   try {
+    const wwwDirectoryPath = `${defaults.nginx.dir.www}/${domain}`
+    await exec(`mkdir ${wwwDirectoryPath}`)
+
+    const nginxConfFilePath = `${defaults.nginx.dir.core}/sites-available/${domain}`
+    const nginxConfFileContent = nginxConfGen({ domain: domain })
+    await exec(`echo "${nginxConfFileContent}" > ${nginxConfFilePath}`)
+
     const _createDomain = await dbRun({
       query: 'insert into domains(domain, createdAt) values($domain, $createdAt)',
       params: {
-        $domain: data.domain,
+        $domain: domain,
         $createdAt: +new Date()
       }
     })
@@ -17,7 +28,7 @@ const createDomain = async (req, res) => {
     result.data = _createDomain.data
     res.json(result)
   } catch (err) {
-    result.error = err.error
+    result.error = err.message
     res.json(result)
   }
 }
