a690a8bb9acc8046da762a1feb732257ea555386
Ozgur Ozer <ozgr@live.com>
2019-10-23 10:43:54 +0300

    Replace connect-sqlite3 with connect-mongo.

--- src/backend/routes/siteRoutes.js
+++ src/backend/routes/siteRoutes.js
@@ -1,23 +1,22 @@
 const path = require('path')
 const express = require('express')
 const session = require('express-session')
-const SQLiteStore = require('connect-sqlite3')(session)
+const MongoStore = require('connect-mongo')(session)
 
 const defaults = require.main.require('./defaults')
 const validatePost = require.main.require('./middleware/validatePost')
-
-const dbPath = `./../../${defaults.site.dbName}.sqlite`
 
 const router = express.Router()
 router.use(express.json())
 router.use(express.static(path.join(__dirname, '..', '..', '..', 'dist')))
 router.use(session({
   resave: true,
+  collection: 'session',
   saveUninitialized: false,
   key: defaults.site.dbName,
   secret: '#%qj#+8dvw8H7U9E',
-  store: new SQLiteStore({
-    db: dbPath
+  store: new MongoStore({
+    url: defaults.site.dbUrl + defaults.site.dbName
   }),
   cookie: { maxAge: 2592000000 }/* 30 days (30 * 24 * 60 * 60 * 1000) */
 }))
