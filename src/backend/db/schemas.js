1f5e11c2b530ad36237f6a500ab18ce677396c90
Ozgur Ozer <ozgr@live.com>
2019-10-23 11:03:33 +0300

    Fix table name.

--- src/backend/db/schemas.js
+++ src/backend/db/schemas.js
@@ -1,5 +1,5 @@
 const schemas = {
-  users: {
+  adminUsers: {
     username: { type: String, required: true },
     password: { type: String, required: true },
     apiKey: { type: String, required: true }
