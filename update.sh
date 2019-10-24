490980e25abb2e7b4184f11da076f5fd7ff572d9
Ozgur Ozer <ozgr@live.com>
2019-10-24 14:34:05 +0300

    Update update.sh file.

--- update.sh
+++ update.sh
@@ -7,14 +7,19 @@ latestVersion=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo "Your version: ${yourVersion}"
 echo "Latest version: ${latestVersion}"
 
-echo "Checking out the latest version"
-git checkout $latestVersion
+if [ $yourVersion == $latestVersion ]
+then
+  echo "You're using the latest version"
+else
+  echo "Checking out the latest version"
+  git checkout $latestVersion
 
-echo "Jaydon: installing dependencies"
-yarn install
+  echo "Jaydon: installing dependencies"
+  yarn install
 
-echo "Jaydon: building React app"
-yarn build
+  echo "Jaydon: building React app"
+  yarn build
 
-echo "Jaydon: restarting server with PM2"
-pm2 restart jaydon
+  echo "Jaydon: restarting server with PM2"
+  pm2 restart jaydon
+fi
