d0e94e7b54e41d5d1b08e3295ad0ec59ac358f07
Ozgur Ozer <ozgr@live.com>
2019-10-30 15:38:40 +0300

    Push index.html after creating it.

--- src/backend/nginx/createGitSupport.js
+++ src/backend/nginx/createGitSupport.js
@@ -3,65 +3,73 @@ const exec = require.main.require('./common/exec')
 const fixContent = require('./fixContent')
 
 const createGitSupport = async props => {
-  const { domain } = props
-  const bareDirectoryPath = `${defaults.git.dir.bare}/${domain}.git`
-  const wwwDirectoryPath = `${defaults.git.dir.www}/${domain}`
+  try {
+    const { domain } = props
+    const bareDirectoryPath = `${defaults.git.dir.bare}/${domain}.git`
+    const wwwDirectoryPath = `${defaults.git.dir.www}/${domain}`
 
-  const postUpdateFilePath = `${bareDirectoryPath}/hooks/post-update`
-  const postCommitFilePath = `${wwwDirectoryPath}/.git/hooks/post-commit`
-  const _postUpdateFileContent = `
-    #!/bin/sh
+    const postUpdateFilePath = `${bareDirectoryPath}/hooks/post-update`
+    const postCommitFilePath = `${wwwDirectoryPath}/.git/hooks/post-commit`
+    const _postUpdateFileContent = `
+      #!/bin/sh
 
-    echo
-    echo "*** Pulling changes into Live"
-    echo
+      echo
+      echo "*** Pulling changes into Live"
+      echo
 
-    cd ${wwwDirectoryPath} || exit
-    unset GIT_DIR
-    git pull hub master
+      cd ${wwwDirectoryPath} || exit
+      unset GIT_DIR
+      git pull hub master
 
-    exec git-update-server-info
-  `
-  const _postCommitFileContent = `
-    #!/bin/sh
+      exec git-update-server-info
+    `
+    const _postCommitFileContent = `
+      #!/bin/sh
 
-    echo
-    echo "*** Pushing changes to Hub"
-    echo
+      echo
+      echo "*** Pushing changes to Hub"
+      echo
 
-    git push hub
-  `
-  const postUpdateFileContent = fixContent({
-    str: _postUpdateFileContent,
-    spaces: 4
-  })
-  const postCommitFileContent = fixContent({
-    str: _postCommitFileContent,
-    spaces: 4
-  })
+      git push hub
+    `
+    const postUpdateFileContent = fixContent({
+      str: _postUpdateFileContent,
+      spaces: 4
+    })
+    const postCommitFileContent = fixContent({
+      str: _postCommitFileContent,
+      spaces: 4
+    })
 
-  const commands = [
-    `mkdir -p ${bareDirectoryPath}`,
-    `cd ${bareDirectoryPath}`,
-    'git init --bare',
+    const commands = [
+      `mkdir -p ${bareDirectoryPath}`,
+      `cd ${bareDirectoryPath}`,
+      'git init --bare',
 
-    `mkdir -p ${wwwDirectoryPath}`,
-    `cd ${wwwDirectoryPath}`,
-    'git init',
-    `git remote add hub ${bareDirectoryPath}`,
+      `mkdir -p ${wwwDirectoryPath}`,
+      `cd ${wwwDirectoryPath}`,
+      'git init',
+      `git remote add hub ${bareDirectoryPath}`,
 
-    `touch ${postUpdateFilePath}`,
-    `echo '${postUpdateFileContent}' > ${postUpdateFilePath}`,
-    `chmod +x ${postUpdateFilePath}`,
+      `touch ${postUpdateFilePath}`,
+      `echo '${postUpdateFileContent}' > ${postUpdateFilePath}`,
+      `chmod +x ${postUpdateFilePath}`,
 
-    `touch ${postCommitFilePath}`,
-    `echo '${postCommitFileContent}' > ${postCommitFilePath}`,
-    `chmod +x ${postCommitFilePath}`
-  ]
+      `touch ${postCommitFilePath}`,
+      `echo '${postCommitFileContent}' > ${postCommitFilePath}`,
+      `chmod +x ${postCommitFilePath}`,
 
-  const joinCommands = commands.join(' && ')
-  const result = await exec(joinCommands)
-  return result
+      'git add .',
+      'git commit -m "Initial commit."',
+      'git push --set-upstream hub master'
+    ]
+
+    const joinCommands = commands.join(' && ')
+    const result = await exec(joinCommands)
+    return result
+  } catch (e) {
+    return e.message
+  }
 }
 
 module.exports = createGitSupport
