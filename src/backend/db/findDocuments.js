53116533244a20130f615f0aad79aae7f779eaf4
Ozgur Ozer <ozgr@live.com>
2019-10-31 10:48:30 +0300

    Fix indentation.

--- src/backend/db/findDocuments.js
+++ src/backend/db/findDocuments.js
@@ -4,27 +4,28 @@ const findDocuments = (opts) => {
   return new Promise((resolve, reject) => {
     const Model = models[opts.model]
 
-    Model.find(opts.find, opts.select, (err, items) => {
-      if (err) {
-        const error = {
-          code: 2,
-          message: 'DB error',
-          detail: err
+    Model
+      .find(opts.find, opts.select, (err, items) => {
+        if (err) {
+          const error = {
+            code: 2,
+            message: 'DB error',
+            detail: err
+          }
+          reject(error)
+        } else {
+          const itemsMap = {}
+
+          items.forEach((item, i) => {
+            itemsMap[i] = item
+          })
+
+          resolve(itemsMap)
         }
-        reject(error)
-      } else {
-        const itemsMap = {}
-
-        items.forEach((item, i) => {
-          itemsMap[i] = item
-        })
-
-        resolve(itemsMap)
-      }
-    })
-    .sort(opts.sort)
-    .limit(opts.limit)
-    .skip(opts.skip)
+      })
+      .sort(opts.sort)
+      .limit(opts.limit)
+      .skip(opts.skip)
   })
 }
 
