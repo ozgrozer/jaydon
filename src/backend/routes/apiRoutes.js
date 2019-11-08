db3f057da63fb8cf29500b367b83e4300cbde0a7
Ozgur Ozer <ozgr@live.com>
2019-11-08 16:42:54 +0300

    Add try catch block.

--- src/backend/routes/apiRoutes.js
+++ src/backend/routes/apiRoutes.js
@@ -10,12 +10,16 @@ router.use(cors())
 const commonConnectApi = require.main.require('./routes/common/connectApi')
 
 router.post('/v1', async (req, res, next) => {
-  const { apiKey, category, event } = req.body.meta
-  const data = req.body.data || {}
-  const version = '1'
-  const meta = { apiKey, category, event, version }
-  const express = { req, res, next }
-  commonConnectApi({ meta, data, express })
+  try {
+    const { apiKey, category, event } = req.body.meta
+    const data = req.body.data || {}
+    const version = '1'
+    const meta = { apiKey, category, event, version }
+    const express = { req, res, next }
+    commonConnectApi({ meta, data, express })
+  } catch (err) {
+    res.json({ success: false })
+  }
 })
 
 module.exports = router
