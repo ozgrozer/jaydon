d8a14658ed765a6bfa7e5adbc88c9af351f6097a
Ozgur Ozer <ozgr@live.com>
2019-11-08 14:32:56 +0300

    Save all cron jobs to disk after update.

--- src/backend/routes/apiRoutes/v1/cronJob/updateCronJob.js
+++ src/backend/routes/apiRoutes/v1/cronJob/updateCronJob.js
@@ -1,4 +1,5 @@
 const { updateDocument } = require.main.require('./db/db')
+const saveAllCronJobsToDisk = require('./saveAllCronJobsToDisk')
 
 const updateCronJobDocument = async props => {
   const { id, data } = props
@@ -17,17 +18,16 @@ const updateCronJob = async (req, res) => {
     const { data } = req.body
     const { id, command, schedule } = data
 
-    const updateDocumentData = {
-      command,
-      schedule
-    }
+    const updateDocumentData = { command, schedule }
     const _updateCronJobDocument = await updateCronJobDocument({
       id,
       data: updateDocumentData
     })
+    result.data = _updateCronJobDocument
+
+    await saveAllCronJobsToDisk()
 
     result.success = true
-    result.data = _updateCronJobDocument
     res.json(result)
   } catch (err) {
     result.error = err.message
