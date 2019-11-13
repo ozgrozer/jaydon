b37a0995d601ca1fa21ddfa278b0d80e3089b0be
Ozgur Ozer <ozgr@live.com>
2019-11-13 14:58:10 +0300

    Update domain document.

--- src/backend/nginx/createSslSupport.js
+++ src/backend/nginx/createSslSupport.js
@@ -1,6 +1,25 @@
-const createSslSupport = props => {
-  const { domain } = props
-  console.log('createSslSupport', domain)
+const { updateDocument } = require.main.require('./db/db')
+
+const updateDomainDocument = async props => {
+  const { domainId } = props
+  const unixTime = Math.round(+new Date() / 1000)
+  const _updateDocument = await updateDocument({
+    model: 'domains',
+    query: { _id: domainId },
+    data: {
+      sslCertificate: {
+        status: 'started',
+        createdAt: unixTime
+      }
+    }
+  })
+  return _updateDocument
+}
+
+const createSslSupport = async props => {
+  const { domain, domainId } = props
+  await updateDomainDocument({ domainId })
+  console.log('createSslSupport', domain, domainId)
 }
 
 module.exports = createSslSupport
