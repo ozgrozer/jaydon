93ac86727a726a74e8475c6298ee227b0fad1297
Ozgur <ozgr@live.com>
2019-11-14 10:42:18 +0300

    Make a certbot demo.

--- src/backend/nginx/createSslSupport.js
+++ src/backend/nginx/createSslSupport.js
@@ -1,3 +1,5 @@
+const defaults = require.main.require('./defaults')
+const exec = require.main.require('./common/exec')
 const { updateDocument } = require.main.require('./db/db')
 
 const updateDomainDocument = async props => {
@@ -16,10 +18,39 @@ const updateDomainDocument = async props => {
   return _updateDocument
 }
 
+const issueCertificate = async props => {
+  const result = { success: false }
+
+  const test = true
+  let runIssuingCertificateCommand = ''
+  if (test) {
+    runIssuingCertificateCommand = await exec('sh /var/www/certbotDemo.sh')
+  } else {
+    const { domain } = props
+    const wwwDirectoryPath = `${defaults.nginx.dir.www}/${domain}`
+    runIssuingCertificateCommand = await exec(`certbot certonly --noninteractive --agree-tos --register-unsafely-without-email --webroot --webroot-path ${wwwDirectoryPath} --domain ${domain} --domain www.${domain}`)
+  }
+
+  const isIssuingSuccessful = /Congratulations/.test(runIssuingCertificateCommand)
+
+  if (isIssuingSuccessful) {
+    result.success = true
+  } else {
+    result.error = runIssuingCertificateCommand
+  }
+
+  return result
+}
+
 const createSslSupport = async props => {
-  const { domain, domainId } = props
-  await updateDomainDocument({ domainId })
-  console.log('createSslSupport', domain, domainId)
+  try {
+    const { domain, domainId } = props
+    await updateDomainDocument({ domainId })
+    const _issueCertificate = await issueCertificate({ domain })
+    console.log(_issueCertificate)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 module.exports = createSslSupport
