4c0ab078d27cf6702c7013966f801e44e6e0d9ee
Ozgur <ozgr@live.com>
2019-11-14 10:32:51 +0300

    Update variable name.

--- src/backend/nginx/nginxConfigurationGenerator.js
+++ src/backend/nginx/nginxConfigurationGenerator.js
@@ -3,13 +3,13 @@ const fixContent = require('./fixContent')
 
 const nginxConfigurationGenerator = props => {
   const { domain } = props
-  const domainDirectory = `${defaults.nginx.dir.www}/${domain}`
+  const wwwDirectoryPath = `${defaults.nginx.dir.www}/${domain}`
 
   const str = `
     server {
       listen 80;
       server_name ${domain} www.${domain};
-      root ${domainDirectory};
+      root ${wwwDirectoryPath};
     }
   `
   const spaces = 4
